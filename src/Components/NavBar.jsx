7747679d142d28eb77f6adfe0861982011dcb45c
Haiyah Perez <haiyahperez@pursuit.org>
2024-06-09 14:48:48 -0400

    landing page margin added

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="bg-gray-800 p-4 fixed top-0 left-0 w-full">
+    <nav className="bg-gray-800 p-4 fixed top-0 left-0 w-full"> 
       <div>
         <Link to="/" style={{ fontSize: '2rem' }} className="text-white text-4xl font-bold dm-serif-text-regular ml-4 mt-4">Nostalgia News</Link>
       </div>
