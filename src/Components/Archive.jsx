7c28e13f2eac50440ba20aae3b1be763fcc595d5
Iris Beckham <irisdancer3@gmail.com>
2024-06-09 10:16:32 -0400

    Archive api token switching

--- src/Components/Archive.jsx
+++ src/Components/Archive.jsx
@@ -1,26 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { ExternalLink } from 'lucide-react';
 
-const API_KEY = import.meta.env.VITE_NYT_API_KEY
+const apiKeys = import.meta.env.VITE_NYT_API_KEY.split(" ");
+let keyIndex = 0;
+
+// const API_KEY = import.meta.env.VITE_NYT_API_KEY
+// const API_KEY_B = import.meta.env.VITE_NYT_API_KEY_B
 
 
 const Archive = ({ searchInputs, loading, setLoading }) => {
   const [data, setData] = useState(null);
-  
+
   const [error, setError] = useState(null);
 
   // const formattedDate = `${searchInputs.year}/${searchInputs.month + 1}`;
   useEffect(() => {
-    // console.log(API_KEY)
-    const fetchData = async () => {
-
-      const url = `https://api.nytimes.com/svc/archive/v1/${searchInputs.year}/${searchInputs.month}.json?api-key=${API_KEY}`;
+    const fetchData = async (keys) => {
+      const url = `https://api.nytimes.com/svc/archive/v1/${searchInputs.year}/${searchInputs.month}.json?api-key=${keys}`;
 
       try {
         setLoading(true); // Set loading state to true before fetching data
         const response = await fetch(url);
         if (!response.ok) {
-          throw new Error(`Error: ${response.status}`);
+          if (response.status === 429) {
+            keyIndex = keyIndex === keysArray.length - 1 ? 0 : keyIndex + 1;
+            fetchData(keysArray[1]);
+          } else
+            throw new Error(`Error: ${response.status}`);
         }
         const result = await response.json();
         setData(result);
@@ -31,7 +37,7 @@ const Archive = ({ searchInputs, loading, setLoading }) => {
       }
     };
 
-    fetchData();
+    fetchData(apiKeys[0]);
   }, [searchInputs]);
 
   if (loading) return <div>Loading...</div>;
@@ -47,14 +53,14 @@ const Archive = ({ searchInputs, loading, setLoading }) => {
         {articleArray.map((article, index) => (
           <li key={index}>
             <h3>Header: {article.headline.main}</h3>
-            <a href={article.web_url} target="_blank"><ExternalLink size={15} className="hover:text-slate-400"/></a>
+            <a href={article.web_url} target="_blank"><ExternalLink size={15} className="hover:text-slate-400" /></a>
             {article.multimedia.length > 0 ? (
               <img
                 src={`https://nytimes.com/${article.multimedia[article.multimedia.length - 1].url}`}
                 alt="article-image"
               />
             ) : (
-              <p>Snippet: {article.snippet ? article.snippet.substring(0, 100) + '...' : article.abstract.substring(0, 100) + '...' } </p>
+              <p>Snippet: {article.snippet ? article.snippet.substring(0, 100) + '...' : article.abstract.substring(0, 100) + '...'} </p>
             )}
           </li>
         ))}
